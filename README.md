1e648e1bf723b311e1750b36a41d0e9566ef23bb
shannon <shannonrmoranetz@gmail.com>
2019-03-05 02:48:14 -0700

    Update package and header names

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# secret project
+# chattle
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <div className="Header">
-      <h1>secret project</h1>
+      <h1>chattle</h1>
     </div>
   )
 }
